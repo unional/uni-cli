299e6e0fab2d7c51cbbc3ce6b93ee342f8de764d
Homa Wong <homawong@gmail.com>
2018-12-17 12:54:18 -0800

    fix: gitignore (#11)
    closes #8

--- packages/uni-cli/src/devpkg-io/copyArtifacts.ts
+++ packages/uni-cli/src/devpkg-io/copyArtifacts.ts
@@ -5,6 +5,7 @@ import { promisify } from 'util';
 import { FolderNotFoundInPackage } from './errors';
 import { tryResolve } from './tryResolve';
 
+import fs from 'fs'
 
 export function copyArtifacts(packageName: string, name: string, cwd = process.cwd()) {
   return copyFile(packageName, path.join(name, 'artifacts'), cwd)
@@ -14,7 +15,8 @@ const ncpp = promisify(ncp)
 export async function copyFile(packageName: string, folder: string, destination: string) {
   const basePath = tryResolve(packageName)
   try {
-    return await ncpp(path.resolve(basePath, folder), destination)
+    await ncpp(path.resolve(basePath, folder), destination)
+    fs.renameSync(path.resolve(destination, '_gitignore'), path.resolve(destination, '.gitignore'))
   }
   catch (errors) {
     // ncpp throws an error array...
