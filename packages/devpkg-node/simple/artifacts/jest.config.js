45727f7c6a704a0b39292768e812876c1b51132a
Homa Wong (unional) <homawong@gmail.com>
2018-12-19 22:57:34 -0800

    fix: clean up eslint config Simplify jest config fix run on node version script

--- packages/devpkg-node/simple/artifacts/jest.config.js
+++ packages/devpkg-node/simple/artifacts/jest.config.js
@@ -1,31 +1,4 @@
-module.exports = {
-  'preset': 'ts-jest',
-  'globals': {
-    'ts-jest': {
-      'diagnostics': false
-    }
-  },
-  'collectCoverageFrom': [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/bin.ts'
-  ],
-  'reporters': [
-    'default',
-    'jest-progress-tracker',
-    ['jest-audio-reporter', { volume: 0.3 }],
-  ],
-  'roots': [
-    '<rootDir>/src'
-  ],
-  'testEnvironment': 'node',
-  'watchPlugins': [
-    'jest-watch-suspend',
-    'jest-watch-repeat',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' }
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' }
-    ]
-  ]
-}
+const common = require('@unional/devpkg-node/simple/config/jest.common')
+module.exports = Object.assign(common, {
+  'testEnvironment': 'node'
+})
